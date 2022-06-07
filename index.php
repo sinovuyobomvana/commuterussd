ac63121128195618b7b7d94d939272124609af39
sinovuyobomvana <106817594+sinovuyobomvana@users.noreply.github.com>
2022-06-07 09:47:58 +0200

    qwwqwq

--- index.php
+++ index.php
@@ -18,9 +18,10 @@ if ($text == "") {
     $response = "CON To pay for your trip please enter the taxi code:\n\n";
     $response .= "0. Back";
     $code = $text;
+    
 } else if($text == "1*1" || $code == $text) { 
     // This is a second level response where the user selected 1 in the first instance
-     $response = "CON You have selected ".$code. " as your taxi from Njoli to Greenacres.\n
+     $response = "CON You have selected ".$code." as your taxi from Njoli to Greenacres.
      Please note that R10 will be deducted from your TeksiPay wallet.\n\n";
      $response .= "1. Confirm\n";
      $response .= "0. Back\n";
