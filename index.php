0a47d513f516dbfdde2b7dfa2e7cb148254fced9
sinovuyobomvana <106817594+sinovuyobomvana@users.noreply.github.com>
2022-06-14 08:58:11 +0200

    1

--- index.php
+++ index.php
@@ -30,8 +30,7 @@ if($userResponse == "XYZ123")
     Please note that R10 will be deducted from your TeksiPay wallet.\n\n";
     $response .= "1. Confirm\n";
     $response .= "0. Back\n";
-} else if($text == "1") { 
-
+} else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
      $response = "CON You have selected 123456 as your taxi from Njoli to Greenacres.
      Please note that R10 will be deducted from your TeksiPay wallet.\n\n";
