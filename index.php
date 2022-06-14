8bd1904ef42f85fc17c938b56c8b08ae4d5988fd
sinovuyobomvana <106817594+sinovuyobomvana@users.noreply.github.com>
2022-06-14 09:50:30 +0200

    1

--- index.php
+++ index.php
@@ -9,32 +9,40 @@ $text        = $_POST["text"];
 $textArray=explode('*', $text);
 $userResponse=trim(end($textArray));
 
-
-if ($text == "") {
-    // This is the first request. Note how we start the response with CON
-    $response  = "CON Welcome to the TBC service. Your balance is R37.50
-    Select your action from the menu below:\n\n";
-    $response .= "1. Pay for a trip\n";
-    $response .= "2. View payment history\n";
-    $response .= "3. Top up wallet";
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON To pay for your trip please enter the taxi code:\n\n";
-    $response .= "0. Back";
+switch ($text) {
+    case '':
+        if ($text == "") {
+            // This is the first request. Note how we start the response with CON
+            $response  = "CON Welcome to the TBC service. Your balance is R37.50
+            Select your action from the menu below:\n\n";
+            $response .= "1. Pay for a trip\n";
+            $response .= "2. View payment history\n";
+            $response .= "3. Top up wallet";
+        } else if ($text == "1") {
+            // Business logic for first level response
+            $response = "CON To pay for your trip please enter the taxi code:\n\n";
+            $response .= "0. Back";
+            
+        } 
+        if($userResponse == "XYZ123")
+        {
+            $response = "CON You have selected ".$userResponse." as your taxi from Njoli to Greenacres.
+            Please note that R10 will be deducted from your TeksiPay wallet.\n\n";
+            $response .= "1. Confirm\n";
+            $response .= "0. Back\n";
+             
+        } 
+        if($text == "1*XYZ123*1") { 
+        
+             $response .= "END Payment successful. Win your share of R4 million in INSTANT cash and airtime with SHOPRITE! Visit your nearest SHOPRITE store and ENTER!";
+        } 
+        break;
     
-} 
-if($userResponse == "XYZ123")
-{
-    $response = "CON You have selected ".$userResponse." as your taxi from Njoli to Greenacres.
-    Please note that R10 will be deducted from your TeksiPay wallet.\n\n";
-    $response .= "1. Confirm\n";
-    $response .= "0. Back\n";
-     
-} 
-if($text == "1*XYZ123*1") { 
+    default:
+    $response .= "END Network Issue, please try again later!";
+        break;
+}
 
-     $response .= "END Payment successful. Win your share of R4 million in INSTANT cash and airtime with SHOPRITE! Visit your nearest SHOPRITE store and ENTER!";
-} 
 
 
 // Echo the response back to the API
